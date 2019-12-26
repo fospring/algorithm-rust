954ac53323b9b96d78420b80c84425e99a72195a
fospring <fospring@outlook.com>
2019-12-26 14:23:56 +0800

    optimize res in recover order

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,6 @@ use std::collections::VecDeque;
 /// \ 8 |          7 底部
 fn sort_dispatch(arr: &[u32]) -> Vec<u32> {
     let mut res = Vec::new();
-//    let mut queue = VecDeque::new();
-//    for elem in arr {
-//        queue.push_back(*elem);
-//    }
-    /// 优化成迭代器使用链式调用方式
     let mut queue: VecDeque<_> = arr.iter().map(|elem| *elem).collect();
     loop {
         if queue.len() > 1 {
@@ -37,6 +32,7 @@ fn sort_dispatch(arr: &[u32]) -> Vec<u32> {
     res
 }
 
+
 /// 恢复牌堆
 ///
 /// 输入value     输入index
@@ -49,13 +45,8 @@ fn sort_dispatch(arr: &[u32]) -> Vec<u32> {
 /// \ 3 |          6
 /// \ 1 |          7 底部
 fn recover_order(arr: &[u32]) -> Vec<u32> {
-//    let mut temp = Vec::with_capacity(arr.len());
-//    arr.clone_into(temp.as_mut());
-//    temp.reverse();
-    /// 优化使用库中封装的方法生成新的Vector
     let mut temp = arr.to_vec();
     temp.reverse();
-    let mut res = Vec::new();
     let mut queue = VecDeque::new();
     loop {
         if temp.len() > 0 {
@@ -74,15 +65,7 @@ fn recover_order(arr: &[u32]) -> Vec<u32> {
         }
     }
 
-    loop {
-        if queue.len() > 0 {
-            let val = queue.pop_front().unwrap();
-            res.push(val);
-        } else {
-            break;
-        }
-    }
-    res
+    queue.iter().map(|val|*val).collect::<Vec<_>>()
 }
 
 fn main() {
