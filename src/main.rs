00665bcd7e44e4d1bda5d2f1d32aed60e16d4176
fospring <fospring@outlook.com>
2019-12-31 10:12:26 +0800

    use VecDeQue.attribte

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,11 @@ use std::collections::VecDeque;
 /// 发牌堆
 /// 输入slice索引越小则在牌堆中的位置越高
 fn sort_dispatch(arr: &[u32]) -> Vec<u32> {
-    let mut res = Vec::new();
-    let mut queue: VecDeque<_> = arr.iter().map(|elem| *elem).collect();
-    loop {
-        if queue.len() > 1 {
-            let val = queue.pop_front().unwrap();
-            res.push(val);
-            let tmp = queue.pop_front().unwrap();
-            queue.push_back(tmp)
-        } else if queue.len() == 1 {
-            let val = queue.pop_front().unwrap();
-            res.push(val);
-            break;
-        } else {
-            break;
-        }
+    let mut res = Vec::with_capacity(arr.len());
+    let mut queue: VecDeque<_> = arr.iter().map(|elem|*elem).collect();
+    while !queue.is_empty() {
+        queue.pop_front().map(|val| res.push(val));
+        queue.pop_front().map(|val|queue.push_back(val));
     }
     res.reverse();
     res
@@ -27,15 +17,14 @@ fn sort_dispatch(arr: &[u32]) -> Vec<u32> {
 /// 恢复牌堆
 /// 输入slice索引越小则在牌堆中的位置越高
 fn recover_order(arr: &[u32]) -> Vec<u32> {
-    let temp = arr.to_vec();
-    let mut queue = VecDeque::new();
-    for val in temp {
+    let mut queue = VecDeque::with_capacity(arr.len());
+    for val in arr.iter() {
         if queue.len() > 0 {
             let bottom: u32 = queue.pop_back().unwrap();
             queue.push_front(bottom);
-            queue.push_front(val);
+            queue.push_front(*val);
         } else {
-            queue.push_front(val);
+            queue.push_front(*val);
         }
     }
 
@@ -43,7 +32,7 @@ fn recover_order(arr: &[u32]) -> Vec<u32> {
 }
 
 fn main() {
-    let input = [1_u32, 2, 3, 4, 5, 6, 7, 8];
+    let input = [1_u32, 2, 3, 4, 5, 6, 7, 8,9];
     let res = sort_dispatch(&input);
     let recover = recover_order(res.as_slice());
     println!("start: {:?}", input);
